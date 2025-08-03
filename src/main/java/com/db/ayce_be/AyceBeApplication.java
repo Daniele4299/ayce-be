6b28162cfa2f55f9c6710e4a9758cd2740aee76d
Daniele4299 <bonadonnadaniele@gmail.com>
2025-08-03 19:05:44 +0200

    Initial commit

---
+++ src/main/java/com/db/ayce_be/AyceBeApplication.java
@@ -0,0 +1,13 @@
+package com.db.ayce_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyceBeApplication.class, args);
+	}
+
+}
